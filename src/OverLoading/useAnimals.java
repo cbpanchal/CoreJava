757e4d4a67e06ecab64f88745be7fd52f596cbcf
hard.chavda <hard.chavda@gmail.com>
2016-08-10 12:34:59 +0530

    just ... and ///

--- src/OverLoading/useAnimals.java
+++ src/OverLoading/useAnimals.java
@@ -12,11 +12,11 @@ class Hourse1 extends Anmil {
 
 class useAnimals {
     public void doStuff(Anmil a) {
-        System.out.println("in the animal version");
+        System.out.println("in the animal version...");
     }
 
     public void doStuff(Hourse1 h) {
-        System.out.println("in the hourse1 version");
+        System.out.println("in the hourse1 version///");
     }
 
     static public void main(String[] argc) {
