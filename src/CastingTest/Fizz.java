56d4578f754931175dc379f42dd966f6061d4cf4
hard.chavda <hard.chavda@gmail.com>
2016-08-17 16:56:24 +0530

    Object Passing as a References...

---
+++ src/CastingTest/Fizz.java
@@ -0,0 +1,25 @@
+package CastingTest;
+
+/**
+ * Created by info on 17-Aug-16.
+ */
+public class Fizz {
+    int x;
+
+    static Fizz FizzSwitch(Fizz x, Fizz y) {
+        final Fizz z = x;
+        z.x = 6;
+        return z;
+
+    }
+
+    public static void main(String[] args) {
+        final Fizz f1 = new Fizz();
+        Fizz f2 = new Fizz();
+        Fizz f3 = FizzSwitch(f1, f2);
+        System.out.println((f1 == f3) + "" + (f1.x == f3.x));
+
+    }
+
+
+}
