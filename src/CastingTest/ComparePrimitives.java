0253ad825ba8dad00beb0657cebdec86dd280ad4
hard.chavda <hard.chavda@gmail.com>
2016-08-19 10:45:44 +0530

    operators returns

---
+++ src/CastingTest/ComparePrimitives.java
@@ -0,0 +1,17 @@
+package CastingTest;
+
+/**
+ * Created by info on 19-Aug-16.
+ */
+public class ComparePrimitives {
+
+
+    public static void main(String[] args) {
+        System.out.println("char 'a'=='a' ?" + ('a' == 'a'));
+        System.out.println("char 'a' =='b'?" + ('a' == 'b'));
+        System.out.println("5!=6? " + (5 != 6));
+        System.out.println("5.0 == 5L ?" + (5.0 == 5L));
+
+
+    }
+}
