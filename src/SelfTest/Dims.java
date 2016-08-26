44c56caad8ab6827bbc8a621e2b1c6012ea2d049
hard.chavda <hard.chavda@gmail.com>
2016-08-26 13:51:35 +0530

    array..

---
+++ src/SelfTest/Dims.java
@@ -0,0 +1,14 @@
+package SelfTest;
+
+/**
+ * Created by info on 26-Aug-16.
+ */
+public class Dims {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3, 4}};
+        int[] b = (int[]) a[1];
+        Object o1 = a;
+        int[][] a2 = (int[][]) o1;
+        System.out.println(b[1]);
+    }
+}
