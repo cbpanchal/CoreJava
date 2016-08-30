a001d70883c7420146dd37d30bdb7b669cc497b6
hard.chavda <hard.chavda@gmail.com>
2016-08-30 12:19:07 +0530

    modified for..

---
+++ src/SelfTest/ForTest.java
@@ -0,0 +1,24 @@
+package SelfTest;
+
+/**
+ * Created by info on 30-Aug-16.
+ */
+public class ForTest {
+
+    /*public static void main(String[] args) {
+        for (int i = 0, j = 0; ((i < 10) && (j < 10)); i++, j++) {
+            System.out.println("i is" + i + " j is " + j);
+
+        }
+    }*/
+
+    public static void main(String[] args) {
+        int b = 3;
+        for (int a = 1; b != 1; System.out.println("iterate")) {
+            b = b - a;
+
+        }
+    }
+
+
+}
