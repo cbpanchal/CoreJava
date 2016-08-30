afc0b1248447eab6c8ebca8364e77803f913ab0e
hard.chavda <hard.chavda@gmail.com>
2016-08-30 12:20:05 +0530

    just ... and ///

--- src/SelfTest/ForTest.java
+++ src/SelfTest/ForTest.java
@@ -5,7 +5,8 @@ package SelfTest;
  */
 public class ForTest {
 
-    /*public static void main(String[] args) {
+    /*declaration..
+    public static void main(String[] args) {
         for (int i = 0, j = 0; ((i < 10) && (j < 10)); i++, j++) {
             System.out.println("i is" + i + " j is " + j);
 
